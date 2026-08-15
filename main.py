004126d3f05be9300e38ceb9c20b5860b479c73a
Igor <ipovaric@gmail.com>
2026-08-15 10:00:28 -0600

    feat: wrapping in main fxn

--- main.py
+++ main.py
@@ -10,29 +10,43 @@ import numpy as np
 
 # inputs (manual)
 # process
-P1 = 100            # pressure at inlet (psia)
-Q1 = 80             # volumetric flow rate at inlet (gpm)
+Pin = 100            # pressure at inlet (psia)
+Qin = 80             # volumetric flow rate at inlet (gpm)
 rho = 62.4          # density of fluid (lbf/ft^3)
 # geometric
 N_pipe = 2          # number of pipe lengths (qty)
 L = [3, 5]          # pipe lengths (ft) [list]
 D = [0.167, 0.167]  # pipe diams (ft) [list]
-i = [0,1]           # pipe order index
+idx = [0,1]         # pipe order index
+
+inputs1 = {'Pin':Pin,
+          'Qin':Qin,
+          'rho':rho,
+          'L':L,
+          'D':D,
+          'idx':idx}
 
 ##########
 
-# intermediate calculations
-# Conv volumetric flow (gpm) to Velocity (ft/s)
-#V = 0.4085 · Q(gpm) / D**2
-V = Q1 * D**2 * 0.104992
+def calcMain(inputs):
 
-f = 0.02 # assumed for now
+    i = 1
+    Di = D(i)
+    Li = L(i)
+    # intermediate calculations
+    # Conv volumetric flow (gpm) to Velocity (ft/s)
+    #V = 0.4085 · Q(gpm) / D**2
+    V = Q1 * Di**2 * 0.104992
 
-# equations
-# Darcy-Weisbach (dP form)
-# ΔP = f · (L/D) · (ρ V² / 2) · (1/144)      [ΔP in psi]
-dP = f * (L/D) * (rho * V**2 / 2) * (1/144)     # [ΔP in psi]
+    f = 0.02 # assumed for now
 
+    # equations
+    # Darcy-Weisbach (dP form)
+    # ΔP = f · (L/D) · (ρ V² / 2) · (1/144)      [ΔP in psi]
+    dP = f * (Li/Di) * (rho * V**2 / 2) * (1/144)     # [ΔP in psi
+
+if __name__ == "__main__":
+    calcMain(inputs)
 
 # unit conversions
 # Volume Flow to Velocity
