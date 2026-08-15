ba25b6303bc82562f7ef8eb4f740fcc5c27aa596
Igor <ipovaric@gmail.com>
2026-08-14 20:37:46 -0600

    initial commit
    Created main file and some reference docs from claude.

---
+++ main.py
@@ -0,0 +1,20 @@
+# Pipe Flow calculator/simulation
+#
+# Igor Povarich, P.E.
+# 8/14/2026
+# 
+import numpy as np
+
+# inputs (manual)
+N_pipe = 2          # number of pipe lengths (qty)
+L = [3, 5]          # pipe lengths (ft) [list]
+D = [0.167 0.167]   # pipe diams (ft) [list]
+
+# equations
+# Darcy-Weisbach (dP form)
+# ΔP = f · (L/D) · (ρ V² / 2) · (1/144)      [ΔP in psi]
+
+
+
+
+ΔP = f · (L/D) · (ρ V² / 2) · (1/144)      [ΔP in psi]
