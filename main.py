0b10aed51aa0f09987f643b25dd5bf6b641c2a51
Igor <ipovaric@gmail.com>
2026-08-15 20:02:52 -0600

    feat: major loss  calc works for 1 component

--- main.py
+++ main.py
@@ -8,18 +8,18 @@
 #
 import numpy as np
 
-# inputs (manual)
+# example inputs (manual)
 # process
-Pin = 100            # pressure at inlet (psia)
-Qin = 80             # volumetric flow rate at inlet (gpm)
+Pin = 100           # pressure at inlet (psia)
+Qin = 120           # volumetric flow rate at inlet (gpm)
 rho = 62.4          # density of fluid (lbf/ft^3)
 # geometric
 N_pipe = 2          # number of pipe lengths (qty)
-L = [3, 5]          # pipe lengths (ft) [list]
+L = [300, 500]      # pipe lengths (ft) [list]
 D = [0.167, 0.167]  # pipe diams (ft) [list]
 idx = [0,1]         # pipe order index
 
-inputs1 = {'Pin':Pin,
+inputs = {'Pin':Pin,
           'Qin':Qin,
           'rho':rho,
           'L':L,
@@ -28,25 +28,37 @@ inputs1 = {'Pin':Pin,
 
 ##########
 
-def calcMain(inputs):
+def calcMajor(inputs):
+    """ Calculate Major (friction) Losses
+    """
+
+    dP = []
 
     i = 1
-    Di = D(i)
-    Li = L(i)
+    Pin     = inputs['Pin']
+    Qin     = inputs['Qin']
+    rho     = inputs['rho']
+    Di      = inputs['D'][i]
+    Li      = inputs['L'][i]
+    idx     = inputs['idx'][i]
+
     # intermediate calculations
     # Conv volumetric flow (gpm) to Velocity (ft/s)
-    #V = 0.4085 · Q(gpm) / D**2
-    V = Q1 * Di**2 * 0.104992
+    V = Qin * Di**2 * 0.104992
 
+    # friction factor calc
     f = 0.02 # assumed for now
 
     # equations
     # Darcy-Weisbach (dP form)
-    # ΔP = f · (L/D) · (ρ V² / 2) · (1/144)      [ΔP in psi]
-    dP = f * (Li/Di) * (rho * V**2 / 2) * (1/144)     # [ΔP in psi
+    dP[i] = f * (Li/Di) * (rho * V**2 / 2) * (1/144)     # [ΔP in psi]
 
-if __name__ == "__main__":
-    calcMain(inputs)
+    print(dP)
+
+calcMajor(inputs)
+
+# if __name__ == "__main__":
+#     calcMain(inputs)
 
 # unit conversions
 # Volume Flow to Velocity
